5da2180c0552e5eb08419fb44dc2c4c9a76103d2
Slava Homenko <hosl_boy@inbox.ru>
2024-10-22 01:23:47 +0300

    accordion

--- src/components/faq-add-info/AddInfo.tsx
+++ src/components/faq-add-info/AddInfo.tsx
@@ -2,27 +2,39 @@ import React, { useState } from "react";
 import cl from './AddInfo.module.css';
 
 interface Info {
-    text: string;
-    main:string;
+    info:InfoItem[];
+}
+interface InfoItem{
+    a:string;
+    b:string;
 }
 
-const AddInfo: React.FC<Info> = ({main,text}) => {
-    const [isActive, setIsActive] = useState<boolean>(false);
+const AddInfo: React.FC<Info> = ({info}) => {
+    const [openId,setOpenId] = useState<number | null>(null);
 
-    const toggleActive = () => {
-        setIsActive(prev => !prev)
-        console.log(isActive)
-    };
+    const toogleActive = (id:number) => {
+        setOpenId(prev => (prev === id ? null : id))
+        console.log(openId)
+    }
+
+
   return (
     <>
-      <div className={cl.content}>
-            <p className={cl.mainText}>{main}</p>
-                <div 
-                    onClick={toggleActive}
-                     className={cl.addInfo}>
-                </div>
-        </div>
-        <p className={cl.addText}>{text}</p>
+        <div className={cl.line}></div>
+        <ul className={cl.accordion}>
+            {info.map((faqItem,id) => {
+                return <li key={id} className={cl.item}>
+                    <div className={cl.content}>
+                        <div className={cl.main}>{faqItem.a}</div>
+                        <div className={`${cl.addInfo} ${openId === id ? cl.exit : ''}`} onClick={() => toogleActive(id)}></div>
+                    </div>
+                    <div className={`${cl.collapse} ${openId === id ? `${cl.open}` : ''}`}>
+                        <div className={cl.addText}>{faqItem.b}</div>
+                    </div>
+                    <div className={cl.line}></div>
+                </li>
+            })}
+        </ul>
     </>
   )
 };
--- src/components/faq/Faq.tsx
+++ src/components/faq/Faq.tsx
@@ -1,19 +1,28 @@
 import AddInfo from '../faq-add-info/AddInfo';
 import cl from './Faq.module.css';
 
+const info = [
+    {
+        a:'How can I track the status of my order?',
+        b:'After placing your order, you will receive a confirmation email containing your order number and a tracking link. You can also log in to your account on our website and go to the "My Orders" section to track your delivery status.'
+    },
+    {
+        a:'What payment methods do you accept?',
+        b:'After placing your order, you will receive a confirmation email containing your order number and a tracking link. You can also log in to your account on our website and go to the "My Orders" section to track your delivery status.'
+    },
+    {
+        a:'How can I return or exchange an item?',
+        b:'After placing your order, you will receive a confirmation email containing your order number and a tracking link. You can also log in to your account on our website and go to the "My Orders" section to track your delivery status.'
+    },
+]
+
 const FAQ = () => {
   return (
     <section className={cl.faq}>
         <div className={cl.container}>
             <h1 className={cl.titleText}>faq</h1>
             <>
-                <div className={cl.line}></div>
-                <AddInfo main={'How can I track the status of my order?'} text={'After placing your order, you will receive a confirmation email containing your order number and a tracking link. You can also log in to your account on our website and go to the "My Orders" section to track your delivery status.'}/>
-                <div className={cl.line}></div>
-                <AddInfo main={'What payment methods do you accept?'} text={'What payment methods do you accept?'}/>
-                <div className={cl.line}></div>
-                <AddInfo main={'How can I return or exchange an item?'} text={'How can I return or exchange an item?'}/>
-                <div className={cl.line}></div>
+                <AddInfo info={info}/>
             </>
         </div>
     </section>
