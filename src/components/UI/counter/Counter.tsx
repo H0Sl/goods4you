cbec5a12abd8f3a35b0650d6ebbfea17b6dbc03e
Slava Homenko <hosl_boy@inbox.ru>
2024-11-02 14:09:26 +0300

    sizeBtn

--- src/components/UI/counter/Counter.tsx
+++ src/components/UI/counter/Counter.tsx
@@ -1,12 +1,17 @@
 import React, { useState } from 'react';
 import { Button } from '../button';
 import cl from './Counter.module.css';
+import cn from 'classnames';
 
 interface CountProps {
     value?: number;
+    size: 'sizeM' | 'sizeL';
 }
 
-export const Counter: React.FC<CountProps> = ({ value = 0 }) => {
+export const Counter: React.FC<CountProps> = ({
+    value = 0,
+    size = 'sizeM',
+}) => {
     const [count, setCount] = useState(value);
     const increment = () => {
         setCount(prevCount => prevCount + 1);
@@ -16,13 +21,14 @@ export const Counter: React.FC<CountProps> = ({ value = 0 }) => {
             setCount(prevCount => prevCount - 1);
         }
     };
+    size = count >= 10 ? 'sizeL' : 'sizeM';
     return (
         <div className={cl.counter}>
-            <Button className={cl.viewBtn} onClick={decrement}>
+            <Button className={cn(cl.viewBtn, cl[size])} onClick={decrement}>
                 <div className={cl.minus} />
             </Button>
             <span>{count} items</span>
-            <Button className={cl.viewBtn} onClick={increment}>
+            <Button className={cn(cl.viewBtn, cl[size])} onClick={increment}>
                 <div className={cl.plus} />
             </Button>
         </div>
