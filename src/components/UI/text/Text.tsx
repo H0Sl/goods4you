60de490ccf7b5aa691daaee7ab471e59d5ed5864
Slava Homenko <hosl_boy@inbox.ru>
2024-11-07 13:34:26 +0300

    New component

---
+++ src/components/UI/text/Text.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import cl from './Text.module.css';
+import cn from 'classnames';
+
+export const Text = () => {
+    return <div />;
+};
---
+++ src/components/UI/title/Title.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import cl from './Title.module.css';
+import cn from 'classnames';
+
+export const Title = () => {
+    return <div />;
+};
