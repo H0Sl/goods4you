94f77270adf91e6f5d97f71718bb5b6cc9788594
Slava Khomenko <hosl_boy@inbox.ru>
2025-02-15 18:50:25 +0300

    Использование ленивой загрузки

--- src/components/PageRouter.tsx
+++ src/components/PageRouter.tsx
@@ -1,39 +1,51 @@
 import { Route, Routes } from 'react-router-dom';
-import { Product } from 'pages/Product';
-import { Home } from 'pages/Home';
-import { Cart } from 'pages/Cart';
-import { Login } from 'pages/Login';
 import { AuthCheck } from './auth-check';
+import { lazy, Suspense } from 'react';
+import { Layout } from './layout/Layout';
+
+const Home = lazy(() => import('pages/Home/Home'));
+const Cart = lazy(() => import('pages/Cart/Cart'));
+const Login = lazy(() => import('pages/Login/Login'));
+const Product = lazy(() => import('pages/Product/Product'));
 
 const PageRouter = () => {
     return (
         <Routes>
-            <Route path="/login" element={<Login />} />
             <Route
-                path="/"
+                path="/login"
                 element={
-                    <AuthCheck>
-                        <Home />
-                    </AuthCheck>
+                    <Suspense fallback={<h1>Loading...</h1>}>
+                        <Login />
+                    </Suspense>
                 }
             />
-            <Route
-                path="/product/:id"
-                element={
-                    <AuthCheck>
-                        <Product />
-                    </AuthCheck>
-                }
-            />
-            <Route
-                path="/cart"
-                element={
-                    <AuthCheck>
-                        <Cart />
-                    </AuthCheck>
-                }
-            />
-            <Route path="/*" element={<Home />} />
+            <Route element={<Layout />}>
+                <Route
+                    path="/"
+                    element={
+                        <AuthCheck>
+                            <Home />
+                        </AuthCheck>
+                    }
+                />
+                <Route
+                    path="/product/:id"
+                    element={
+                        <AuthCheck>
+                            <Product />
+                        </AuthCheck>
+                    }
+                />
+                <Route
+                    path="/cart"
+                    element={
+                        <AuthCheck>
+                            <Cart />
+                        </AuthCheck>
+                    }
+                />
+                <Route path="/*" element={<Home />} />
+            </Route>
         </Routes>
     );
 };
